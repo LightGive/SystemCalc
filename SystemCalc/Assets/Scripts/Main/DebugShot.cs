e8db72c3f3ec81838f8d4a7534cb79464317ca96
12LJJY1\User <roi.akase@outlook.jp>
2017-04-17 22:07:18 +0900

    GetVelocityTopTime(初速を加えて何秒後に頂点に達するかを求める関数)完成

---
+++ SystemCalc/Assets/Scripts/Main/DebugShot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugShot : MonoBehaviour
+{
+	[SerializeField]
+	private Vector3 vec;
+
+	private Rigidbody rigid;
+
+	void Start ()
+	{
+		rigid = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+	{
+		//スペースキーを押したときに球を飛ばす
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			rigid.velocity = vec;
+			StartCoroutine(WaitBreak(SystemCalc.GetVelocityTopTime(vec)));
+		}
+	}
+
+	private IEnumerator WaitBreak(float _waitTime)
+	{
+		yield return new WaitForSeconds(_waitTime);
+		Debug.Log(_waitTime);
+		Debug.Break();
+	}
+}
---
+++ SystemCalc/Assets/Scripts/SystemCalc.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 様々な計算を扱うクラス
+/// </summary>
+public static　class SystemCalc
+{
+	/// <summary>
+	/// 通常の重力加速度
+	/// </summary>
+	private const float DefaultGravitationalAcceleration = -9.80665f;
+	/// <summary>
+	/// 通常の空気抵抗
+	/// </summary>
+	private const float DefaultDrag = 0.0f;
+	/// <summary>
+	/// 通常の質量
+	/// </summary>
+	private const float DefaultMass = 1.0f;
+	/// <summary>
+	/// 通常の重力加速度のベクトル
+	/// </summary>
+	private static readonly Vector3 DefaultGravitationalAccelerationVec = new Vector3(0.0f, DefaultGravitationalAcceleration, 0.0f);
+
+	#region GetVelocityTopTime (初速を加えて何秒後に頂点に達するかを求める)
+
+	/// <summary>
+	/// 初速を加えて何秒後に頂点に達するかを求める
+	/// </summary>
+	/// <param name="_vec">初速</param>
+	/// <returns>何秒後に頂点に到達するか</returns>
+	public static float GetVelocityTopTime(float _vec)
+	{
+		return GetVelocityTopTime(_vec, DefaultGravitationalAccelerationVec);
+	}
+
+	/// <summary>
+	/// 初速を加えて何秒後に頂点に達するかを求める
+	/// </summary>
+	/// <param name="_vec">初速</param>
+	/// <returns>何秒後に頂点に到達するか</returns>
+	public static float GetVelocityTopTime(Vector3 _vec)
+	{
+		return GetVelocityTopTime(_vec, DefaultGravitationalAccelerationVec);
+	}
+
+	/// <summary>
+	/// 初速を加えて何秒後に頂点に達するかを求める
+	/// </summary>
+	/// <param name="_vec">初速</param>
+	/// <param name="_gravity">重力加速度</param>
+	/// <returns>何秒後に頂点に到達するか</returns>
+	public static float GetVelocityTopTime(Vector3 _vec, Vector3 _gravity)
+	{
+		return GetVelocityTopTime(_vec.y, _gravity);
+	}
+
+	/// <summary>
+	/// 初速を加えて何秒後に頂点に達するかを求める
+	/// </summary>
+	/// <param name="_vec">初速</param>
+	/// <param name="_gravity">重力加速度</param>
+	/// <returns>何秒後に頂点に到達するか</returns>
+	public static float GetVelocityTopTime(Vector3 _vec, float _gravity = DefaultGravitationalAcceleration)
+	{
+		return GetVelocityTopTime(_vec.y, new Vector3(0.0f, _gravity, 0.0f));
+	}
+
+	/// <summary>
+	/// 初速を加えて何秒後に頂点に達するかを求める
+	/// </summary>
+	/// <param name="_vec">初速</param>
+	/// <param name="_gravity">重力加速度</param>
+	/// <returns>何秒後に頂点に到達するか</returns>
+	public static float GetVelocityTopTime(float _vec, Vector3 _gravity, float _mass = DefaultMass, float _drag = DefaultDrag)
+	{
+		return Mathf.Clamp(_vec / -_gravity.y, 0.0f, float.PositiveInfinity);
+	}
+
+	#endregion
+
+
+}
