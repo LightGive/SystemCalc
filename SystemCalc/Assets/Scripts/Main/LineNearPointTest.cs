451e25319721f07a980b01b699582a3e9b61ec72
MatsuuraAkase <roi.akase@outlook.jp>
2017-06-15 13:11:56 +0900

    線と点の線状の最短座標を求める

---
+++ SystemCalc/Assets/Scripts/Main/LineNearPointTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineNearPointTest : MonoBehaviour {
+
+	[SerializeField]
+	private Transform p1;
+	[SerializeField]
+	private Transform p2;
+	[SerializeField]
+	private Transform nearPoint;
+	[SerializeField]
+	private Transform gizmos;
+	[SerializeField]
+	private LineRenderer line;
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		line.SetPosition(0, p1.position);
+		line.SetPosition(1, p2.position);
+
+		gizmos.position = SystemCalc.GetLineNearPoint(p1.position, p2.position, nearPoint.position);
+	}
+}
