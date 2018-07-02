bc1c3cab473da5fcaa6195ec691e163c66eb046e
LightGive <roi.akase@outlook.jp>
2018-07-02 19:51:05 +0900

    線状で一番近い座標を取得する処理作成

--- SystemCalc/Assets/LightGive/SystemCalc/Examples/Scripts/Main/Example3.cs
+++ SystemCalc/Assets/LightGive/SystemCalc/Examples/Scripts/Main/Example3.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-
-public class Example3 : MonoBehaviour {
-
+public class Example3 : MonoBehaviour
+{
 	[SerializeField]
 	private Transform m_p1;
 	[SerializeField]
@@ -16,13 +16,20 @@ public class Example3 : MonoBehaviour {
 	[SerializeField]
 	private LineRenderer m_line;
 
-
+	[SerializeField]
+	private Text m_textValue;
+	[SerializeField]
+	private Toggle m_toggleIsLimit;
 
 	void Update ()
 	{
 		m_line.SetPosition(0, m_p1.position);
 		m_line.SetPosition(1, m_p2.position);
 
-		m_dragPoint.position = SystemCalc.GetLineNearPoint(m_p1.position, m_p2.position, m_nearPoint.position, true);
+		var lerp = 0.0f;
+		var nearPos = SystemCalc.GetLineNearPoint(m_p1.position, m_p2.position, m_nearPoint.position, m_toggleIsLimit.isOn, out lerp);
+		nearPos.z = -1;
+		m_dragPoint.position = nearPos;
+		m_textValue.text = lerp.ToString("F2");
 	}
 }
--- SystemCalc/Assets/LightGive/SystemCalc/Examples/Scripts/PositionDrag.cs
+++ SystemCalc/Assets/LightGive/SystemCalc/Examples/Scripts/PositionDrag.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PositionDrag : MonoBehaviour
 {
+	[SerializeField]
+	private float m_zPosition = 0.0f;
+
 	private bool m_isDrag;
 
 	private void OnMouseDown()
@@ -19,7 +22,7 @@ public class PositionDrag : MonoBehaviour
 	private void OnMouseDrag()
 	{
 		var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		pos.z = 0.0f;
+		pos.z = m_zPosition;
 		transform.position = pos;
 	}
 }
