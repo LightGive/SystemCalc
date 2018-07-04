d66a6417ed49ee65773a4ab412aa6b7651cc51c0
LightGive <roi.akase@outlook.jp>
2018-07-04 14:48:28 +0900

    シーン修正

--- SystemCalc/Assets/LightGive/SystemCalc/Examples/Scripts/Example6.cs
+++ SystemCalc/Assets/LightGive/SystemCalc/Examples/Scripts/Example6.cs
@@ -16,10 +16,28 @@ public class Example6 : MonoBehaviour
 	[SerializeField]
 	private Vector3 m_ballResetPos;
 
-	void Update ()
+	void Update()
 	{
-		m_topPoint.position = SystemCalc.GetVelocityTopPos(
-			new Vector3(float.Parse(m_inputFieldVelocity_x.text), float.Parse(m_inputFieldVelocity_y.text)),
-			m_ballResetPos);
+
+		float resultX = 0.0f;
+		float resultY = 0.0f;
+		if (float.TryParse(m_inputFieldVelocity_x.text, out resultX) && float.TryParse(m_inputFieldVelocity_y.text, out resultY))
+		{
+			m_topPoint.position = SystemCalc.GetVelocityTopPos(
+					new Vector3(float.Parse(m_inputFieldVelocity_x.text), float.Parse(m_inputFieldVelocity_y.text)),
+					m_ballResetPos);
+		}
+	} 
+
+	public void OnButtonDownSimulate()
+	{
+		if (m_inputFieldVelocity_x.text == ""||m_inputFieldVelocity_y.text == "")
+			return;
+
+		m_ballRigid.simulated = false;
+		m_ballRigid.transform.position = m_ballResetPos;
+		m_ballRigid.velocity = Vector2.zero;
+		m_ballRigid.simulated = true;
+		m_ballRigid.AddForce(new Vector2(float.Parse(m_inputFieldVelocity_x.text), float.Parse(m_inputFieldVelocity_y.text)), ForceMode2D.Impulse);
 	}
 }
