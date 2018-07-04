67169b984f279db892dc17eec37d3efbcd57c8dd
LightGive <roi.akase@outlook.jp>
2018-07-04 13:35:06 +0900

    Exampleシーン追加

---
+++ SystemCalc/Assets/LightGive/SystemCalc/Examples/Scripts/Example6.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Example6 : MonoBehaviour
+{
+	[SerializeField]
+	private InputField m_inputFieldVelocity_x;
+	[SerializeField]
+	private InputField m_inputFieldVelocity_y;
+	[SerializeField]
+	private Transform m_topPoint;
+	[SerializeField]
+	private Rigidbody2D m_ballRigid;
+	[SerializeField]
+	private Vector3 m_ballResetPos;
+
+	void Update ()
+	{
+		
+	}
+}
