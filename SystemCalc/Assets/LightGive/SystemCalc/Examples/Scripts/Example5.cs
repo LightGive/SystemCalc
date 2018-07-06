f34e70f40297b51b162793ca86dc0d7bb410c782
LightGive <roi.akase@outlook.jp>
2018-07-06 17:26:37 +0900

    不要なExampleを削除

---
+++ SystemCalc/Assets/LightGive/SystemCalc/Examples/Scripts/Example5.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Example5: MonoBehaviour
+{
+	[SerializeField]
+	private InputField m_inputFieldVelocity_x;
+	[SerializeField]
+	private InputField m_inputFieldVelocity_y;
+	[SerializeField]
+	private Transform m_topPoint;
+	[SerializeField]
+	private Rigidbody2D m_ballRigid;
+	[SerializeField]
+	private Vector3 m_ballResetPos;
+
+	void Update()
+	{
+
+		float resultX = 0.0f;
+		float resultY = 0.0f;
+		if (float.TryParse(m_inputFieldVelocity_x.text, out resultX) && float.TryParse(m_inputFieldVelocity_y.text, out resultY))
+		{
+			m_topPoint.position = SystemCalc.GetVelocityTopPos(
+					new Vector3(float.Parse(m_inputFieldVelocity_x.text), float.Parse(m_inputFieldVelocity_y.text)),
+					m_ballResetPos);
+		}
+	} 
+
+	public void OnButtonDownSimulate()
+	{
+		if (m_inputFieldVelocity_x.text == ""||m_inputFieldVelocity_y.text == "")
+			return;
+
+		m_ballRigid.simulated = false;
+		m_ballRigid.transform.position = m_ballResetPos;
+		m_ballRigid.velocity = Vector2.zero;
+		m_ballRigid.simulated = true;
+		m_ballRigid.AddForce(new Vector2(float.Parse(m_inputFieldVelocity_x.text), float.Parse(m_inputFieldVelocity_y.text)), ForceMode2D.Impulse);
+	}
+}
